63dfcd6508d55609e661d1544d6b379c4393cdb7
undstory <undrul.ozog@gmail.com>
2020-12-09 17:01:45 +0100

    styles added tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 const todoAddBtn = document.querySelector(".todo__add--btn");
 const todoAdd = document.querySelector(".todo__add");
+const todoList = document.querySelector('.todo__list');
+const todosStatusItems = document.querySelector('.todos__status--items');
 
-
+const todos = [];
 
 class Todo  {
     constructor(todo) {
@@ -11,15 +13,27 @@ class Todo  {
 
     addTodo(task) {
         this.todo = task;
-        const todoList = document.querySelector('.todo__list');
         const div = document.createElement('div');
         div.classList.add("todo__item");
-        div.innerHTML = `<p>${this.todo}</p>`;
+        div.innerHTML = `<div><button class="circle"></button>
+                        <span>${this.todo}</span>
+                        </div>
+                        <button class="close"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+                        <path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 
+                        8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 
+                        .707.707 0 8.84 8.132 16.971 0z"/></svg></button>`;
         todoList.appendChild(div);
+        todos.push(this.todo);
+        console.log(todos);
     }
 
     resume() {
-        return this.todo;
+        todosStatusItems.innerText = `${todos.length} items left`;
+      
+    }
+
+    clearInput() {
+        todoAdd.value = "";
     }
 
 }
@@ -31,7 +45,10 @@ todoAddBtn.addEventListener('click', function() {
     console.log(value);
     if(value !== ""){
         todoApp.addTodo(value);
+        todoApp.resume();
 
+    } else {
+        alert("Todo is empty!");
     }
 
 })
@@ -42,7 +59,10 @@ todoAdd.addEventListener('keypress', function(e) {
 
         if(value !== ""){
             todoApp.addTodo(value);
-    
+            todoApp.resume();
+            todoApp.clearInput();
+        } else {
+            alert("Todo is empty!");
         }
     }
 
