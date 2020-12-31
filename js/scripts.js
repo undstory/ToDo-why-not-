ec95c55d33ae327d74551ee18242b99632d30fe7
undstory <undrul.ozog@gmail.com>
2020-12-31 13:44:40 +0100

    filters is working

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,67 @@ addBtn.addEventListener('click', function() {
     const todosCircle = document.createElement("div");
     const cancelBtn = document.createElement('div');
 
+    paragraph.classList.add("todo__item");
     paragraph.classList.add("todos");
     paragraph.innerText = todoInput.value;
 
+    todosCircle.classList.add("todos-circle");
     cancelBtn.classList.add("todos-cancel");
     cancelBtn.innerText = "X";
 
     paragraph.appendChild(todosCircle);
     paragraph.appendChild(cancelBtn);
-    todoList.appendChild(paragraph);  
+    todoList.appendChild(paragraph); 
+    
+    todoInput.value = "";
+
+    countItem();
+
+    todosCircle.addEventListener('click', function() {
+        todosCircle.classList.toggle("clicked");
+        paragraph.classList.toggle("checked");
+        countItem();
+    })
+
+    cancelBtn.addEventListener("click", function() {
+        this.parentElement.remove();
+        countItem();
+    })
+})
+
+function countItem() {
+    const itemCount = todoList.childElementCount;
+    const checkedElement = document.querySelectorAll(".checked").length;
+    const unCheckedCount = itemCount - checkedElement;
+    itemsCounter.innerText = unCheckedCount + " items left"; 
+}
+
+document.querySelector(".clear-completed").addEventListener("click", function() {
+    document.querySelectorAll(".checked").forEach(element => {
+        element.remove();
+    })
+})
+
+document.querySelector(".all").addEventListener("click", function() {
+    document.querySelectorAll(".todos").forEach(element => {
+        element.style.display = "flex";
+    })
+})
+
+document.querySelector(".completed").addEventListener("click", function() {
+    document.querySelectorAll(".checked").forEach(element => {
+        element.style.display = "flex";        
+    })
+    document.querySelectorAll(".todos:not(.checked)").forEach(element => {
+        element.style.display = "none";
+    })
+})
+
+document.querySelector(".active").addEventListener("click", function() {
+    document.querySelectorAll(".checked").forEach(element => {
+        element.style.display = "none";        
+    })
+    document.querySelectorAll(".todos:not(.checked)").forEach(element => {
+        element.style.display = "flex";
+    })
 })
